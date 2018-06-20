06f6201409b23aee9ecfc627bb496c627b1e33c8
TB831 <druu831@gmail.com>
2018-06-20 10:20:36 -0700

    maxchar complete. 2 solutions

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,38 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {}
+  str.split('').map(char => {
+    chars[char] = chars[char] + 1 || 1
+  });
+  return Object.keys(chars).reduce((a, b) => {
+    return chars[a] > chars[b] ? a : b;
+  });
+}
+
+// Solution #2
+// function maxChar(str) {
+//   const chars = {}  // Empty Object
+//   let max = 0;  // Value of Object
+//   let maxChar = ''; // Key to Value
+//
+//   for (let char of str) { // Loop to iterate through the str
+//     if (chars[char]) {  // If Char in Chars exists
+//       chars[char]++;  // Increment
+//     } else {
+//       chars[char] = 1;  // Set to 1
+//     }
+//   }
+//
+//   for (let char in chars) { // Loop specific to iterate through an object. Char becomes keys of the object
+//     if (chars[char] > max) {  // If Char is > Max
+//       max = chars[char];  // Set Max to that value
+//       maxChar = char; // Set MaxChar to that key
+//     }
+//   }
+//
+//   return maxChar;
+// }
 
 module.exports = maxChar;
