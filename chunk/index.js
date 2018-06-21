56716466b7a33fb0bce6c73b0bf350680e99bead
TB831 <druu831@gmail.com>
2018-06-20 17:09:31 -0700

    chunk complete. 2 solutions

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,28 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = []; // Empty array
+  for (let element of array) {
+    const last = chunked[chunked.length - 1]; // Grabs last element of chunked
+    if (!last || last.length === size) { // If last doesnt exist || last length === size
+      chunked.push([element]); // Push new chunk and current element
+    } else {
+      last.push(element); // Push last element
+    }
+  }
+  return chunked;
+}
+
+// Solution #2
+// function chunk(array, size) {
+//   const chunked = []; // Empty array
+//   let index = 0;  // Index counter
+//   while(index < array.length) { // While index is < array length
+//     chunked.push(array.slice(index, index + size)); // Push slice of array from index to index+size
+//     index += size;  // Increment the index by adding size
+//   }
+//   return chunked;
+// }
 
 module.exports = chunk;
