4aeb10ecb970ff6ce4ca4a4d5b365e9718609a74
TB831 <druu831@gmail.com>
2018-06-25 13:48:35 -0700

    pyramid complete. iterative solution

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,21 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const midpoint = Math.floor((2 * n - 1) / 2); // Midpoint of a level ex. [0, 1, 2, 3, 4] = 2
+  for (let row = 0; row < n; row++) { // Iterate through n number of rows
+    let level = ''; // Empty string for levels
+    for (let col = 0; col < 2 * n - 1; col++) { // Iterate through 2*n-1
+      if (midpoint - row <= col && midpoint + row >= col) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level);
+  }
+}
+
+pyramid(3);
 
 module.exports = pyramid;
