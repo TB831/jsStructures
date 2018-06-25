1b200526ad6817f50cbdd413d437a2c53398fa33
TB831 <druu831@gmail.com>
2018-06-25 16:17:06 -0700

    vowels complete. 2 solutions

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let vowelCounter = 0; // Initiate counter to 0
+  const vowelsChecker = ['a', 'e', 'i', 'o', 'u'];  // Array of vowels to check against
+  str.toLowerCase().split('').map(char => { // toLowerCase. Convert to charArray. Loop over each char.
+    if (vowelsChecker.includes(char)) { // If char is in vowelsChecker
+      vowelCounter++; // Increment
+    }
+  });
+  return vowelCounter;
+}
+
+// function vowels(str) {
+//   const matches = str.match(/[aeiou]/gi); // Regex to match chars 'aeiou'. /g checks the whole string. /i Ignores case sensitivite, capitals/non capitals
+//   return matches ? matches.length : 0;  // Ternary statement
+// }
 
 module.exports = vowels;
