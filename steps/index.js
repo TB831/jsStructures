6785554b1ea86304978d71ebfde9cbe12f475a4a
TB831 <druu831@gmail.com>
2018-06-24 18:27:32 -0700

    steps complete. 2 solutions

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,38 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let row = 0; row < n; row++) { // Iterate through n number of rows
+    let stair = ''; // Empty string
+    for (let col = 0; col < n; col++) { // Iterate through n number of cols
+      if (col <= row) { // If
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+}
+
+// Solution #2
+// function steps(n, row=0, stair='') {
+//   if (n === row) {
+//     return;
+//   }
+//   if (n === stair.length) { // When we hit the end of a row console.log stairs
+//     console.log(stair);
+//     return steps(n, row+1); // Increment row
+//   }
+//
+//   if (stair.length <= row) {  // When stair.length is <= row we add #
+//     stair += '#';
+//   } else {
+//     stair += ' '; // Else space
+//   }
+//
+//   steps(n, row, stair);
+// }
+
 
 module.exports = steps;
