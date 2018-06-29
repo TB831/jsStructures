2422db331a81c30f3bf8d2f89a32f7626b7e3743
TB831 <druu831@gmail.com>
2018-06-29 10:24:09 -0700

    stack complete

--- stack/index.js
+++ stack/index.js
@@ -10,6 +10,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.data = []; // Empty array
+  }
+
+  push(record) {
+    this.data.push(record); // Pushes a record from the stack;
+  }
+
+  pop() {
+    return this.data.pop(); // Pop a record from the stack;
+  }
+
+  peek() {
+    return this.data[this.data.length - 1]; // Access the last element without removing
+  }
+}
 
 module.exports = Stack;
