22dceac23110d582c356f72ba26f31416f43d1c0
TB831 <druu831@gmail.com>
2018-07-02 11:13:17 -0700

    Node class complete

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -2,8 +2,15 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next=null) {  // Default next to null, to represent tail of node
+    this.data = data; // Data assoiciated with the node
+    this.next = next; // Reference to next node in the list
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+
+}
 
 module.exports = { Node, LinkedList };
