6383e08162f3c93285471402569d98b4eeaac3ce
TB831 <druu831@gmail.com>
2018-07-02 12:34:43 -0700

    methods size, getFirst, getLast complete

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -17,6 +17,33 @@ class LinkedList {
   insertFirst(data) { // Adds a new node to the linked list
     this.head = new Node(data, this.head) //  Points head to newely added node
   }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+    while (node) {  // If a node exist
+      counter++;
+      node = node.next; // Look onto the next node, if Null break
+    }
+    return counter;
+  }
+
+  getFirst() {
+    return this.head; // Return the first node of the linked list
+  }
+
+  getLast() {
+    if (!this.head) { // If there is no node
+      return null;  // Return null
+    }
+    let node = this.head;
+    while (node) {  // If a node exist
+      if (!node.next) { // If there is no next node, then were at the end of the list
+        return node;  // Return ending node
+      }
+      node = node.next; // Continue through next node
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
