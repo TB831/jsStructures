c5c4714ad5b9c6546d4dd88571888d7c58b0b31f
TB831 <druu831@gmail.com>
2018-07-03 10:04:30 -0700

    circular complete

--- circular/index.js
+++ circular/index.js
@@ -12,6 +12,18 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (fast.next && fast.next.next ) {  // While there is 2 & 1 nodes ahead of fast
+    slow = slow.next; // Move slow to the next node
+    fast = fast.next.next;  // Move fast to the next 2 nodes
+    if (slow === fast) {  // Compare slow node to fast node
+      return true;  // If equal, list is circular
+    }
+  }
+  return false; // If the while loop ever breaks, we do not have a circular list
+}
 
 module.exports = circular;
