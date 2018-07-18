219b50eb3256b17ae2f38883e8eed83cb524c571
TB831 <druu831@gmail.com>
2018-07-18 16:18:51 -0700

    bst complete

--- bst/index.js
+++ bst/index.js
@@ -29,6 +29,19 @@ class Node {
       this.right = new Node(data);  // Create a new node below the right most node
     }
   }
+
+  contains(data) {
+    if (this.data === data) { // If current node equals searched node
+      return this;  // Returns current node
+    }
+
+    if (this.data < data && this.right) { // If current node < searched node and there is a right node present
+      return this.right.contains(data); // Recurse and continue down left side of the tree till found data
+    } else if (this.data > data && this.left) { // If current node > searched node and there is a right node present
+      return this.left.contains(data);  // Recurse and continue down right side of the tree till found data
+    }
+    return null;  // Checks failed, no matching node
+  }
 }
 
 module.exports = Node;
