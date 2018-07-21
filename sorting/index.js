8ad19618c6446ba750f3160833ad6d6771f8138c
TB831 <druu831@gmail.com>
2018-07-21 12:14:04 -0700

    selectionSort complete

--- sorting/index.js
+++ sorting/index.js
@@ -11,11 +11,24 @@ function bubbleSort(arr) {
       }
     }
   }
-  return arr;
+  return arr; // Return sorted array
 }
 
 function selectionSort(arr) {
-
+  for (let i=0; i<arr.length; i++) {  // Loop over entire collection
+    let indexOfMin = i; // Assumption of lowest value at current index
+    for (let j=i+1; j<arr.length; j++) {  // Inner loop to i+1
+      if (arr[j] < arr[indexOfMin]) { // If theres an element < indexOfMin
+        indexOfMin = j; // Assign that element to indexOfMin
+      }
+    }
+    if (indexOfMin !== i) { // If index of current element and index of lowest element aren't equal, swap them
+      let lesser = arr[indexOfMin]; // Temp variable to hold lesser index
+      arr[indexOfMin] = arr[i]; // Swap indexOfMin with i;
+      arr[i] = lesser;  // Swap i with lesser(indexOfMin)
+    }
+  }
+  return arr; // Return sorted array
 }
 
 function mergeSort(arr) {
